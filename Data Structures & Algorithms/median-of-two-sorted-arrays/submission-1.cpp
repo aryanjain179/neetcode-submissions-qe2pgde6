19a31113d8b30300cbb3b029621d720221251263
Aryan Jain <107005819+aryanjain179@users.noreply.github.com>
2026-03-31 12:32:34 -0500

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int left = (m + n + 1) / 2;  // middle for odd, first middle for even
+        int right = (m + n + 2) / 2; // second middle for even
+
+        // get the median by finding k-th elements
+        return (getKth(nums1, nums2, left) + getKth(nums1, nums2, right)) / 2.0;
+    }
+
+    int getKth(vector<int>& a, vector<int>& b, int k) {
+        int aStart = 0, bStart = 0;
+
+        while (true) {
+            // base cases
+            if (aStart >= a.size()) return b[bStart + k - 1];
+            if (bStart >= b.size()) return a[aStart + k - 1];
+            if (k == 1) return min(a[aStart], b[bStart]);
+
+            // take k/2-th element in each array (or last if not enough elements)
+            int aMidIndex = min((int)a.size() - aStart, k / 2);
+            int bMidIndex = min((int)b.size() - bStart, k / 2);
+
+            int aMidValue = a[aStart + aMidIndex - 1];
+            int bMidValue = b[bStart + bMidIndex - 1];
+
+            if (aMidValue <= bMidValue) {
+                // discard first aMidIndex elements of a
+                aStart += aMidIndex;
+                k -= aMidIndex;
+            } else {
+                // discard first bMidIndex elements of b
+                bStart += bMidIndex;
+                k -= bMidIndex;
+            }
+        }
+    }
+};
